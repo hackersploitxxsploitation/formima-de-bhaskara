e2f77e2fa5ad8294b61e5a9688872d213e86b3f7
Hackersploit__xxSploitation <78088921+hackersploitxxsploitation@users.noreply.github.com>
2021-01-27 12:14:25 -0300

    Add files via upload
    instalação:
    apt update && apt upgrade
    pkg install git -y
    pkg install python3 -y
    git clone "aqui vc adiciona o link"
    ls
    python3 "Arquivo

---
+++ bhaskara.py
@@ -0,0 +1,23 @@
+# fórmula de bhaskara
+print ("digite os valores da sua equação:")
+a=float(input ())
+b=float(input ())
+c=float (input ())
+delta=b**2-4*a*c
+x=(-b+(delta)**0.5)/2*a
+y=(-b-(delta)**0.5)/2*a
+
+
+ 
+ 
+ 
+
+if (delta <0):
+  print(" sua equação não possui soluções reais ")
+
+else:
+  
+  print ("A solução da equação é ")
+  print(x)
+  print(y)
+  # produzido Hackersploit__xxSploitation
